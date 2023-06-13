fae43ba9e8885d87058e2778c0be697a1eeee760
Bianca Oliveira <babianca.sete@gmail.com>
2023-06-13 20:20:23 -0300

    first commit

---
+++ exemplo/src/assets/pages/home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css';
+
+
+function Home(){
+    return(
+      <>
+
+  <h1 className='titulo'>  Bem Vindes ao meu Futuro Blog Pessoal </h1>
+
+<img src="./assets/images/Bianca_Oliveira.jpeg" alt="Mulher Linda Esbelta (Bianca Oliveira)" className='img' />
+
+  </>
+  
+    )
+}
+
+export default Home;

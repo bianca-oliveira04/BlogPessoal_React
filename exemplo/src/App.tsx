9b344884b7d6c67ad4bfe6be9e8511251555c8d4
Bianca Oliveira <babianca.sete@gmail.com>
2023-06-20 14:10:13 -0300

    adicionando Login

--- exemplo/src/App.tsx
+++ exemplo/src/App.tsx
@@ -1,18 +1,27 @@
-import './App.css'
-import Home from './assets/pages/home';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
+import Home from './pages/Home/home.tsx';
+import './App.css';
+import Login from './pages/login/Login.tsx';
 
 
 function App() {
   return (
-    <>
-      
+    <Router>
       <Navbar />
-       <Home />
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
-      
-    </>
+    </Router>
+
   );
 }
 
--- exemplo/src/main.tsx
+++ exemplo/src/main.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
-import Home from './assets/pages/home.tsx'
-
+import Home from './pages/Home/home.tsx'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') as HTMLElement
+   document.getElementById('root') as HTMLElement
 )
-
-
-
-
-

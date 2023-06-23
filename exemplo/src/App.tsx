4eafb46b2e4dc22cbb2749106e0a1ca1ef269e24
Bianca Oliveira <babianca.sete@gmail.com>
2023-06-23 11:48:43 -0300

    Implementação do login, get all temas e postagens

--- exemplo/src/App.tsx
+++ exemplo/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
-import Home from './pages/Home/home.tsx';
+import Home from './pages/Home/home';
 import './App.css';
-import Login from './pages/login/Login.tsx';
-
+import Login from './pages/login/Login';
+import CadastroUsuario from './pages/CadastroUsuario/CadastroUsuario';
+import ListaTema from './componentes/tema/listatema/ListaTema';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
 
 function App() {
   return (
@@ -13,11 +15,19 @@ function App() {
       <Navbar />
       <Routes>
 
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login  />} />
 
         <Route path="/home" element={<Home />} />
 
         <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/posts" element={<ListaPostagem />} />
+
+
       </Routes>
       <Footer />
     </Router>
